04026180d610727b1810803b2447afc642381bf4
Bulana <Bulana@hal9000.zapper.local>
2019-05-24 15:41:20 +0200

    added radio buttons

--- DroidDesserts/app/src/main/java/com/bulana/droiddesserts/OrderActivity.java
+++ DroidDesserts/app/src/main/java/com/bulana/droiddesserts/OrderActivity.java
@@ -1,22 +1,50 @@
 package com.bulana.droiddesserts;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class OrderActivity extends AppCompatActivity {
-    private String message;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
 
         Intent intent = getIntent();
-        message = "Order: "+ intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         TextView textview = findViewById(R.id.order_textview);
         textview.setText(message);
     }
 
+    public void onRadioButtonClicked(View view) {
+        boolean checked = ((RadioButton) view).isChecked();
+
+        switch (view.getId()) {
+            case R.id.sameday:
+                if (checked) {
+                    displayToast(getString(R.string.same_day_messenger_service));
+                }
+                break;
+            case R.id.nextday:
+                if (checked)
+                    displayToast(getString(R.string.next_day_ground_delivery));
+                break;
+            case R.id.pickup:
+                if (checked)
+                    displayToast(getString(R.string.pick_up));
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
 }
