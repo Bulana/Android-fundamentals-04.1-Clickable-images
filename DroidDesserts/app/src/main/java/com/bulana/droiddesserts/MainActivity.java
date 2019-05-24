8d0c90595aeb0e1eeec336cf4de66c586edc5e79
Bulana <Bulana@hal9000.zapper.local>
2019-05-24 11:04:43 +0200

    code challenge complete

--- DroidDesserts/app/src/main/java/com/bulana/droiddesserts/MainActivity.java
+++ DroidDesserts/app/src/main/java/com/bulana/droiddesserts/MainActivity.java
@@ -3,7 +3,6 @@ package com.bulana.droiddesserts;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -12,6 +11,8 @@ import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.bulana.android.droid.extra.MESSAGE";
+    private String mOrderMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,25 +56,29 @@ public class MainActivity extends AppCompatActivity {
      * Shows a message that the donut image was clicked.
      */
     public void showDonutOrder(View view) {
-        displayToast(getString(R.string.donut_order_message));
+        mOrderMessage = getString(R.string.donut_order_message);
+        displayToast(mOrderMessage);
     }
 
     /**
      * Shows a message that the ice cream sandwitch image as clicked.
      */
     public void showIceCreamOrder(View view) {
-        displayToast(getString(R.string.ice_cream_order_message));
+        mOrderMessage = getString(R.string.ice_cream_order_message);
+        displayToast(mOrderMessage);
     }
 
     /**
      * Shows a message that the froyo image was clicked.
      */
     public void showFroyoOrder(View view) {
-        displayToast(getString(R.string.froyo_order_message));
+        mOrderMessage = getString(R.string.froyo_order_message);
+        displayToast(mOrderMessage);
     }
 
     public void onClick(View view) {
         Intent intent = new Intent(MainActivity.this, OrderActivity.class);
+        intent.putExtra(EXTRA_MESSAGE, mOrderMessage);
         startActivity(intent);
     }
 
--- DroidDesserts/app/src/main/java/com/bulana/droiddesserts/OrderActivity.java
+++ DroidDesserts/app/src/main/java/com/bulana/droiddesserts/OrderActivity.java
@@ -1,14 +1,22 @@
 package com.bulana.droiddesserts;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class OrderActivity extends AppCompatActivity {
-
+    private String message;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+
+        Intent intent = getIntent();
+        message = "Order: "+ intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        TextView textview = findViewById(R.id.order_textview);
+        textview.setText(message);
     }
 
 }
